a744fa1e7336d25343ce9c592cf7297b238a5727
Mubasshir Ahmed <mubasshirahmed26@gmail.com>
2017-11-11 12:31:25 +0600

    Project Added

---
+++ UnityScrollview/Assets/Scripts/BowlerButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BowlerButton : MonoBehaviour
+{
+    private int _bowlerID;
+    public Image bowlerImage;
+    public Text bowlerName;
+
+    public void ValueSetup(BowlerInfo bowlerInfo)
+    {
+        _bowlerID = bowlerInfo.bowlerID;
+        bowlerImage.sprite = bowlerInfo.bowlerImage;
+        bowlerName.text = bowlerInfo.bowlerName;
+    }
+
+    public void OnButtonClick()
+    {
+        Debug.Log(_bowlerID);
+        Debug.Log(bowlerName.text.ToString());
+    }
+}
---
+++ UnityScrollview/Assets/Scripts/BowlerScrollList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class BowlerInfo
+{
+    public int bowlerID;
+    public Sprite bowlerImage;
+    public string bowlerName;
+}
+
+public class BowlerScrollList : MonoBehaviour
+{
+    public List<BowlerInfo> bowlerInfoList;
+    public Transform contentList;
+    public GameObject bowlerInfoBtn;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < bowlerInfoList.Count; i++)
+        {
+            GameObject newBtn = Instantiate(bowlerInfoBtn);
+            newBtn.transform.SetParent(contentList);
+            BowlerButton bowlerButton = newBtn.GetComponent<BowlerButton>();
+            bowlerButton.ValueSetup(bowlerInfoList[i]);
+        }
+    }
+}
